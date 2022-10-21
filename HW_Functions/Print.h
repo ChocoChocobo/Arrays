00674ca50ddd9694a3fe9888181078c15ab22168
ChocoChocobo <sharonovjr@yahoo.com>
2022-10-21 20:13:07 +0300

    final templated separate

--- HW_Functions/Print.h
+++ HW_Functions/Print.h
@@ -1,7 +1,30 @@
 #pragma once
-#include"Constants.h"
+#include "Constants.h"
+#include "stdafx.h"
 
 template<typename T>
 void Print(const int n, T arr[]);
 template<typename T>
 void Print(T arr[ROWS][COLS], const int ROWS, const int COLS);
+
+template<typename T>
+void Print(const int n, T arr[])
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "   ";
+	}
+}
+
+template<typename T>
+void Print(T arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
